e306daf27b09a079082452a56e905509b5a8c718
Tristan Muscat <tristan.muscat@gmail.com>
2024-12-01 11:02:44 +0100

    Small changes and corrections in scripts.

--- dotfiles/.config/zsh/completion.bash
+++ dotfiles/.config/zsh/completion.bash
@@ -1,7 +1,8 @@
 #/usr/bin/env bash
 # Just a collection of auto completion commands for my bash custom scripts.
 
-complete -W "mono multi" chdis
+complete -W "mono multi main-off" chdis
 complete -W "fr uk down" proton
 complete -W "up down" rpivpn
 complete -W "-clean -month -archive -location" notes
+complete -W "aqua bonsai cava matrix" anim
