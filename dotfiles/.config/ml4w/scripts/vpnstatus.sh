80e3f1e8e43f8038ffa893779660b4d4f95bc612
Tristan Muscat <tristan.muscat@gmail.com>
2024-12-07 21:42:27 +0100

    Improved zsh symlinks. Improved vpn display module for waybar.

--- dotfiles/.config/ml4w/scripts/vpnstatus.sh
+++ dotfiles/.config/ml4w/scripts/vpnstatus.sh
@@ -6,10 +6,11 @@ status=$(cat /home/tristan/.config/vpnstatus)
 # Generate JSON based on the status
 if [[ "$status" == "VPN > "* ]]; then
     connection="${status#VPN > }"
-    echo "{\"text\": \"VPN: $connection\", \"tooltip\": \"Connected to $connection\", \"alt\": \"connected\"}"
+    echo "{\"text\": \"VPN : $connection\", \"tooltip\": \"Connected to $connection via Wireguard\", \"alt\": \"connected\"}"
 elif [[ "$status" == "Proton > "* ]]; then
     connection="${status#Proton > }"
-    echo "{\"text\": \"ProtonVPN: $connection\", \"tooltip\": \"Connected to ProtonVPN - $connection\", \"alt\": \"connected\"}"
+    connection_upper="${connection^^}" # Convert to uppercase
+    echo "{\"text\": \"Proton : $connection_upper\", \"tooltip\": \"Connected to Proton VPN - $connection_upper\", \"alt\": \"connected\"}"
 elif [[ "$status" == "VPN Down" ]]; then
     echo "{\"text\": \"\", \"alt\": \"off\"}"
 else
